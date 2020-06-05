febd3596c537107d4b496952ae59272adb914d1e
ermteri <ermteri@gmail.com>
2020-06-05 13:05:03 +0200

    Fixed automatic headers

--- src/merg_csv_files.py
+++ src/merg_csv_files.py
@@ -5,19 +5,31 @@ import sys
 import csv
 import re
 
-
+# net_manager,purchase_area,street,zipcode_from,zipcode_to,city,delivery_perc,num_connections,perc_of_active_connections,
+# type_conn_perc,type_of_connection,annual_consume,annual_consume_lowtarif_perc,smartmeter_perc
 def open_csv_file(result, csv_file):
     '''
     :param csv_file: the file to open
     :return: the file content as a list of csv records
     '''
     year = re.search('.*(....).csv', csv_file).group(1)
+    energy_type = 'None'
+    if 'Gas' in csv_file:
+        energy_type = 'Gas'
+    elif 'Electricity' in csv_file:
+        energy_type = 'Electricity'
+
     with open(csv_file, newline='') as csvfile:
         reader = csv.DictReader(csvfile)
+        # Append year and energy_type to the new csv
         for row in reader:
             row['year'] = year
-            result.append(row)
-    return result
+            row['energy_type'] = energy_type
+            result.append(dict(row))
+    header = list(reader.fieldnames)
+    header.append('year')
+    header.append('energy_type')
+    return result, header
 
 
 def run(args):
@@ -26,9 +38,12 @@ def run(args):
     args = parser.parse_args()
     result = list()
     for csvfile in args.csvfiles:
-        open_csv_file(result, csvfile)
-    for row in result:
-        print(row['year'],row['annual_consume'])
+        result, field_names = open_csv_file(result, csvfile)
+    header_form = ','.join(['{}' for i in range(len(field_names))])
+    print(field_names)
+    for row in sorted(result, key=lambda i: (i['year'], i['energy_type'])):
+        out = row.values()
+        print(header_form.format(*out))
 
 
 if __name__ == '__main__':
